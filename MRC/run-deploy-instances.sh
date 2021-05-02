eca618a9b10a1f6e8c3f4e292bdeb2b5b9694770
yjc95 <yujiacheng10@gmail.com>
2021-05-02 15:37:20 +1000

    setup all 4 instance and update all apt packages. Then will work on specific packages

---
+++ MRC/run-deploy-instances.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+. ./team-61-openrc.sh; ansible-playbook --ask-become-pass deploy_instances.yaml
--- MRC/run-nectar.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-. ./team-61-openrc.sh; ansible-playbook --ask-become-pass mrc.yaml
